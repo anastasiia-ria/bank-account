c2ae53f73a1bd6ba1c1b8b1773a7460c421b989d
Liam Eller <liamthelastson@gmail.com>
2022-01-25 13:07:17 -0800

    Add UI logic and html with styligng to format form
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@ BankList.prototype.assignId = function() {
   return this.currentId;
 };
 
-AddressBook.prototype.findAccount = function(id) {
-  if (this.account[id] != undefined) {
+BankList.prototype.findAccount = function(id, name) {
+  if (this.account[id] != undefined ||  this.account.name === name) {
     return this.account[id];
   }
   return false;
@@ -26,3 +26,32 @@ function BankAccount(name, balance) {
   this.name = name;
   this.balance = balance;
 }
+
+BankAccount.prototype.operations = function(number) {
+  this.balance += number;
+}
+
+function login(userId, userName) {
+  account = findAccount(userId, userName);
+  if (account.name === userName) {
+    showAccountDetails(userId);
+  } else {
+    $("#error-message").show().delay(800);
+  }
+}
+
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    const registerName = $('input#name').val();
+    const initialDeposit = $('input#initial-deposit').val();
+    const loginName = $('input#login-name').val();
+    const accountNumber = $('input#account-number').val();
+    const operation = $('input[name="operation"]:checked').val();
+    const amount = $('input#amount').val();
+
+    $('input#name').val("");
+    $('input#initial-deposit').val("");
+    $('input#login-name').val("");
+  });
+});
